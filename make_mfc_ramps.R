6d7a400ecf48d8e89e8e1e0f380ae1f0fba3bbd7
Alberto Rovellini <alberto.rovellini@gmail.com>
2025-07-28 21:14:37 -0700

    added skeleton for mfc ramp script

---
+++ make_mfc_ramps.R
@@ -0,0 +1,14 @@
+# Alberto Rovellini
+# 07/28/2025
+# Code to create mFC ramps for reference point simulations
+library(tidyverse)
+
+# read in FMSY vecs from old paper
+
+# filter 5 stocks of interest
+
+# make ramp
+
+# create multiplier from starting point PRM
+
+# write PRM files
